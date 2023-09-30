5fbcdf48e064075ec760ea24274e10f195e1edda
Bruno Alehtxandre <brunoaleh1@gmail.com>
2023-09-30 11:46:58 -0300

    estrutura de pastas exemplo

---
+++ src/app/clone-page/components/ButtonPage/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+export const ButtonPage = () => (
+  <button
+    type="button"
+    className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  >
+    Tailwind
+  </button>
+);
--- src/app/clone-page/page.tsx
+++ src/app/clone-page/page.tsx
@@ -1,16 +1,44 @@
 "use client";
 
-import { Box, Stack, Heading, Text } from "@chakra-ui/react";
+import {
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Card,
+  CardBody,
+  CardFooter,
+} from "@chakra-ui/react";
+import { ButtonPage } from "./components/ButtonPage";
 
 export default function ClonePage() {
   return (
-    <Box pt={4} bg={"#8a7f7f"}>
-      <Stack direction="column" align="center" justify="center" py={10}>
-        <Heading color={"#333"} size="xl" as="h1">
-          Como usar Chakra
-        </Heading>
-        <Text>Testando Pagina</Text>
+    <Card
+      direction={{ base: "column", sm: "row" }}
+      overflow="hidden"
+      variant="outline"
+    >
+      <Image
+        objectFit="cover"
+        maxW={{ base: "100%", sm: "200px" }}
+        src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+        alt="Caffe Latte"
+      />
+
+      <Stack>
+        <CardBody>
+          <Heading size="md">The perfect latte</Heading>
+
+          <Text py="2">
+            Caffè latte is a coffee beverage of Italian origin made with
+            espresso and steamed milk.
+          </Text>
+        </CardBody>
+
+        <CardFooter>
+          <ButtonPage />
+        </CardFooter>
       </Stack>
-    </Box>
+    </Card>
   );
 }
