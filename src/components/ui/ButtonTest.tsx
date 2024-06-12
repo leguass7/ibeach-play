8ca16f4445481b9c7f88043ce7286764dfd44112
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2024-06-11 22:10:00 -0300

    docs: configura shadcn

---
+++ src/components/ui/ButtonTest.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from '@chakra-ui/react'
+
+type Props = {}
+
+export const ButtonTest: React.FC<Props> = () => {
+  return <Button colorScheme="blue">Button</Button>
+}
