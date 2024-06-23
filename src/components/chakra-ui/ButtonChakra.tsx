5698567ee57834033dec43a9b4c5dc7b79e02288
Bruno Alehtxandre <brunoaleh1@gmail.com>
2024-06-23 12:27:59 -0300

    Button chakra tipado e com estilo padrão

---
+++ src/components/chakra-ui/ButtonChakra.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Button, ButtonProps as ButtonPropsChakra } from '@chakra-ui/react'
+
+interface ButtonProps extends ButtonPropsChakra {
+  colorScheme?: 'whiteAlpha' | 'blackAlpha' | 'gray' | 'red' | 'orange' | 'yellow' | 'green' | 'teal' | 'blue' | 'cyan' | 'purple' | 'pink'
+  size?: 'lg' | 'md' | 'sm' | 'xs'
+  variant?: 'ghost' | 'outline' | 'solid' | 'link' | 'unstyled'
+  onClick?: () => void
+  children?: React.ReactNode
+  textButton?: string
+}
+
+export const ButtonChakra: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  colorScheme = 'gray',
+  size = 'md',
+  variant = 'solid',
+  textButton = 'Button',
+  ...rest
+}) => {
+  const hasChild = children ? true : false
+  return (
+    <Button onClick={onClick} {...rest}>
+      {hasChild ? children : textButton?.toLocaleUpperCase()}
+    </Button>
+  )
+}
