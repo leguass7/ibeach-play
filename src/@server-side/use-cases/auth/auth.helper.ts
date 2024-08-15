b8f24d666dbfc8c29bd485d2eef1ddc820d4d29f
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2024-08-15 00:02:48 -0300

    feat(deps): adiciona SWR

--- src/@server-side/use-cases/auth/auth.helper.ts
+++ src/@server-side/use-cases/auth/auth.helper.ts
@@ -8,11 +8,21 @@ export type JwtOrSession = (JWT | Session) & { level?: number }
 
 export function sessionToAuthorizedDto(data?: Session | null): IAuthorizedUser | null {
   if (!data?.user?.id) return null
-  return { userId: data?.user?.id, name: data?.user?.name || '', email: data?.user?.email || '', groups: data?.groups || [] }
+  return {
+    userId: data?.user?.id,
+    name: data?.user?.name || '',
+    email: data?.user?.email || '',
+    groups: data?.groups || []
+  }
 }
 
 export function tokenToAuthorizedDto(data?: JWT | null): IAuthorizedUser | null {
   const userId = data?.sub ? +data?.sub || 0 : 0
   if (userId) return null
-  return { userId, name: data?.name || '', email: data?.email || '', groups: data?.groups || [] }
+  return {
+    userId,
+    name: data?.name || '',
+    email: data?.email || '',
+    groups: data?.groups || []
+  }
 }
