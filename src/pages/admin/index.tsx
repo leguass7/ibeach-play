3575d6a708f0a8204aefaaaf53708a95ed20bfb0
Bruno Alehtxandre <brunoaleh1@gmail.com>
2024-08-17 14:16:59 -0300

    melhorando distribuição de pastas

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next/types'
+
+type Props = {}
+
+const AdminDashPage: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+    </div>
+  )
+}
+
+export default AdminDashPage
---
+++ src/pages/partner/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next/types'
+
+type Props = {}
+
+const PartnerDashPage: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h1>Partner Dashboard</h1>
+    </div>
+  )
+}
+
+export default PartnerDashPage
