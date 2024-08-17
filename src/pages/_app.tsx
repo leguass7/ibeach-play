573149959173a39c6fe1bdd34005d7978912a841
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2024-08-17 14:51:45 -0300

    feat: adiciona storage, redux e persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,26 @@
 import '@/styles/globals.css'
+import { Provider as ReduxProvider } from 'react-redux'
+
+import { persistor, store } from '@/store'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { PersistGate } from 'redux-persist/integration/react'
 import { SWRConfig } from 'swr'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <SWRConfig value={{ fetcher: (resource, init) => fetch(resource, init).then(res => res.json()) }}>
-      <ChakraProvider>
-        <SessionProvider>
-          <Component {...pageProps} />
-        </SessionProvider>
-      </ChakraProvider>
-    </SWRConfig>
+    <ReduxProvider store={store}>
+      <PersistGate persistor={persistor}>
+        <SWRConfig value={{ fetcher: (resource, init) => fetch(resource, init).then(res => res.json()) }}>
+          <ChakraProvider>
+            <SessionProvider>
+              <Component {...pageProps} />
+            </SessionProvider>
+          </ChakraProvider>
+        </SWRConfig>
+      </PersistGate>
+    </ReduxProvider>
   )
 }
 
